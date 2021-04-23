f41ce6b0667f875c646d9e19c21620f295bec3cc
chatastaff <83054570+chatastaff@users.noreply.github.com>
2021-04-23 17:51:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/EXCuvNd`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/RncqFEMVBn`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("755636700482633791").send(
-`> <:ARBELE_UPUP:791411050238836766> By: <@${message.author.id}> ✅ 
+  client.channels.get("784066297066422314").send(
+`By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
     }
@@ -57,4 +57,4 @@ client.user.setGame(`ڕیکلامەکە بنێرە بۆمن دایدەنێم☑�
 
 
 
-client.login("");//تۆکین لێرە دانێ
+client.login("ODM1MTY0ODEzMzc4MTkxNDMx.YILd2A.RJ2tN1IwA6sYGDsSEK4yBldOfyk");//تۆکین لێرە دانێ
